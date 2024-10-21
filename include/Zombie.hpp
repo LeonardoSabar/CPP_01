a11efbcd35960623e1ecfa894c69c648b56c888f
Leonardo Barbosa Sales <leonardosabar@gmail.com>
2024-10-20 23:07:42 -0300

    Setup init

---
+++ include/Zombie.hpp
@@ -0,0 +1,20 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <iostream>
+
+class Zombie
+{
+    private:
+        std::string _name;
+        std::string _type;
+    public:
+        Zombie(void);
+        ~Zombie(void);
+        
+        void announce(void);
+        void setName(std::string name);
+        void setType(std::string type);
+};
+
+#endif
