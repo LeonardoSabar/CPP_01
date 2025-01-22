2d3ac0e3793f980ab3add4526009a7685bf7e858
Leonardo Sales <leonardosabar@gmail.com>
2025-01-22 14:11:00 +0000

    ex01 em Setup

--- ex01/src/Zombie.cpp
+++ ex01/src/Zombie.cpp
@@ -6,29 +6,31 @@
 /*   By: leobarbo <leobarbo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 12:40:35 by leobarbo          #+#    #+#             */
-/*   Updated: 2025/01/22 13:36:03 by leobarbo         ###   ########.fr       */
+/*   Updated: 2025/01/22 14:09:48 by leobarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie* zombieHorde(int N, std::string name)
+Zombie::Zombie(void) : _name("Unnamed Zombie")
 {
-    if (N <= 0)
-    {
-        std::cerr << "Error: Invalid number of zombies." << std::endl;
-        return NULL;
-    }
+}
 
-    Zombie *zombies = new (std::nothrow) Zombie[N];
-    if (!zombies)
-    {
-        std::cerr << "Error: Memory allocation failed." << std::endl;
-        return NULL;
-    }
-    for (int i = 0; i < N; i++)
-    {
-        zombies[i].setName(name);
-    }
-    return zombies;
+Zombie::Zombie(std::string name) : _name(name)
+{
+}
+
+void Zombie::announce(void)
+{
+    std::cout << _name << ": " << G << "BraiiiiiiinnnzzzZ..." << RST << std::endl;
+}
+
+void Zombie::setName(std::string name)
+{
+    this->_name = name;
+}
+
+Zombie::~Zombie(void)
+{
+    std::cout << _name << " is destroyed." << std::endl;
 }
--- ex01/src/zombieHorde.cpp
+++ ex01/src/zombieHorde.cpp
@@ -6,13 +6,13 @@
 /*   By: leobarbo <leobarbo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 00:33:50 by leobarbo          #+#    #+#             */
-/*   Updated: 2025/01/22 13:44:07 by leobarbo         ###   ########.fr       */
+/*   Updated: 2025/01/22 14:08:12 by leobarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie *zombieHorde(int N, std::string name)
+Zombie* zombieHorde(int N, std::string name)
 {
     if (N <= 0)
     {
