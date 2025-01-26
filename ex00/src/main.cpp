b768cd7546e470100ecd664a69f94269891ef1b9
Leonardo Barbosa Sales <leobarbo@c1r7p1.42sp.org.br>
2025-01-26 14:46:55 -0300

    ok

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: leobarbo <leobarbo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 00:34:32 by leobarbo          #+#    #+#             */
-/*   Updated: 2025/01/26 11:47:07 by leobarbo         ###   ########.fr       */
+/*   Updated: 2025/01/26 14:43:37 by leobarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int main()
 {
 	Zombie* heapZombie = newZombie("HeapZombie");
+	heapZombie->announce();
 	delete heapZombie;
 
 	randomChump("StackZombie");
--- ex00/src/newZombie.cpp
+++ ex00/src/newZombie.cpp
@@ -6,7 +6,7 @@
 /*   By: leobarbo <leobarbo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 00:11:03 by leobarbo          #+#    #+#             */
-/*   Updated: 2025/01/26 12:11:57 by leobarbo         ###   ########.fr       */
+/*   Updated: 2025/01/26 14:44:35 by leobarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,5 @@ Zombie* newZombie(std::string _name)
 		return NULL;
 	}
 	zombie->setName(_name);
-	zombie->announce();
 	return zombie;
 }
